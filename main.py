4ac643cfa824f6c0615ac189be96f6e6e58a1bd8
kiyimba1 <edwineddie54@gmail.com>
2020-01-23 18:35:58 +0300

    Fix main

--- main.py
+++ main.py
@@ -11,23 +11,17 @@ from models import Entry
 
 @app.route('/latest.atom')
 def recent_feed():
-    feed = AtomFeed(
-        'Latest Blog Posts',
-        feed_url = request.url,
-        url = request.url_root,
-        author = request.url_root
-    )
-    entries = Entry.query.filter(Entry.status == 
-        Entry.STATUS_PUBLIC).order_by(Entry.created_timestamp.desc()).limit(25).all()
-        for entry in entries:
-            feed.add(
-                entry.title,
-                entry.body,
-                content_type='html',
-                url=urljoin(request.url_root, url_for("entries.detail", slug=entry.slug) ),
-                updated = entry.modified_timestamp,
-                published = entry.created_timestamp
-            )
+    feed = AtomFeed('Latest Blog Posts', feed_url = request.url, url = request.url_root, author = request.url_root )
+    entries = Entry.query.filter(Entry.status == Entry.STATUS_PUBLIC).order_by(Entry.created_timestamp.desc()).limit(25).all()
+    for entry in entries:
+        feed.add(
+            entry.title,
+            entry.body,
+            content_type='html',
+            url=urljoin(request.url_root, url_for("entries.detail", slug=entry.slug) ),
+            updated = entry.modified_timestamp,
+            published = entry.created_timestamp
+        )
     return feed.get_response()
 
 from entries.blueprint import entries
